31fb737263808700615d8fbc3bfd0b8d66c76ea6
Mattias Jadelius <mattias.jadelius@gmail.com>
2011-05-14 17:05:09 +0200

    Added focus on project and back again (shift+return/shift+backspace)

--- taskpaper.el
+++ taskpaper.el
@@ -2,6 +2,7 @@
 (when (not taskpaper-mode-map)
   (setq taskpaper-mode-map (make-sparse-keymap))
   (define-key taskpaper-mode-map (kbd "<S-return>") 'taskpaper-focus-project)
+  (define-key taskpaper-mode-map (kbd "<S-backspace>") 'taskpaper-unfocus-project)
   )
 
 (setq tpKeywords
@@ -13,6 +14,31 @@
 
   )
 )
+
+(defun taskpaper-focus-project()
+  "Hide everything not related to current project."
+  (interactive)
+  (let ((startpoint (point)) start end)
+    (end-of-line)
+    (re-search-backward ":$")
+    (beginning-of-line)
+    (setq start (point))
+    (re-search-forward ":$")
+    (re-search-forward ":$")
+    (beginning-of-line)
+    (setq end (point))
+    (add-text-properties 1 start '(invisible t))
+    (add-text-properties end (point-max) '(invisible t))
+    (goto-char startpoint)
+    )
+  )
+
+(defun taskpaper-unfocus-project()
+  "Show all projects if focused on one."
+  (interactive)
+  (add-text-properties 1 (point-max) '(invisible nil))
+  )
+
 (defun taskpaper-mode ()
   "Major mode for editing taskpaper styled files."
   (interactive)
