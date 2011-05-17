081afc95f9d252c8413a11d1edd9aaa5db8ef09a
Mattias Jadelius <mattias.jadelius@gmail.com>
2011-05-17 21:23:02 +0200

    Fix toggle done, minor cleanup

--- taskpaper.el
+++ taskpaper.el
@@ -5,127 +5,130 @@
   (define-key taskpaper-mode-map (kbd "<S-backspace>") 'taskpaper-unfocus-project)
   (define-key taskpaper-mode-map (kbd "C-c d") 'taskpaper-toggle-done)
   (define-key taskpaper-mode-map (kbd "C-c l") 'taskpaper-chose-project)
-  
   )
 
 (setq tpKeywords
- '(
-   (".*@done.*" . font-lock-comment-face)
-   (".*:$" . font-lock-function-name-face)
-   ("^ *[^- ].*[^:]$" . font-lock-comment-face)
-   ("@.*" . font-lock-variable-name-face)
-
-  )
-)
-
-(defun taskpaper-chose-project()
-  "Show a list of projects to chose from."
-  (interactive)
-  (let ((projects '()) (startpoint (point)) (x 0) (buffer (concat (buffer-name) ": Projects")))
-    (goto-char 0)
-    ;;Gather list of projects
-    (while (re-search-forward ":$" nil t)
-      (back-to-indentation)
-      (setq projects (append projects (list (buffer-substring-no-properties (point) (- (line-end-position) 1)))))
-      (end-of-line)
+      '(
+        (".*@done.*" . font-lock-comment-face)
+        (".*:$" . font-lock-function-name-face)
+        ("^ *[^- ].*[^:]$" . font-lock-comment-face)
+        ("@.*" . font-lock-variable-name-face)
+        
+        )
       )
-    (goto-char startpoint)
 
-    (when (get-buffer buffer) 
+(defun taskpaper-functions()
+
+  (defun taskpaper-chose-project()
+    "Show a list of projects to chose from."
+    (interactive)
+    (let ((projects '()) (startpoint (point)) (x 0) (buffer (concat (buffer-name) ": Projects")))
+      (goto-char 0)
+      ;;Gather list of projects
+      (while (re-search-forward ":$" nil t)
+        (back-to-indentation)
+        (setq projects (append projects (list (buffer-substring-no-properties (point) (- (line-end-position) 1)))))
+        (end-of-line)
+        )
+      (goto-char startpoint)
+
+      (when (get-buffer buffer) 
         (kill-buffer buffer))
-    ;;Open popupwindow for selecting project
-    (split-window-vertically (- (+ (length projects) 1)))
-    (other-window 1)
-    (get-buffer-create buffer)
-    (switch-to-buffer buffer)
-    (while (< x (length projects))
-      (insert (elt projects x))
-      (insert "\n")
-      (setq x (+ x 1))
+      ;;Open popupwindow for selecting project
+      (split-window-vertically (- (+ (length projects) 1)))
+      (other-window 1)
+      (get-buffer-create buffer)
+      (switch-to-buffer buffer)
+      (while (< x (length projects))
+        (insert (elt projects x))
+        (insert "\n")
+        (setq x (+ x 1))
+        )
+      (goto-char 0)
       )
-    (goto-char 0)
+    (toggle-read-only t)
+    (local-set-key (kbd "<return>") 'taskpaper-projectwindow-select)
+    (local-set-key (kbd "<ESC> <ESC>") 'taskpaper-projectwindow-esc)  
     )
-  (toggle-read-only t)
-  (local-set-key (kbd "<return>") 'taskpaper-projectwindow-select)
-  (local-set-key (kbd "<ESC> <ESC>") 'taskpaper-projectwindow-esc)  
-  )
 
-(defun taskpaper-projectwindow-esc()
-  "Exit projectwindow, not selecting a project"
-  (interactive)
-  (local-unset-key (kbd "<return"))
-  (local-unset-key (kbd "<ESC> <ESC>"))
-  (kill-buffer)
-  (delete-window)
-  (other-window (- 1))
-  )
-
-(defun taskpaper-projectwindow-select()
-  "Action to perform when project is selected in project window."
-  (interactive)
-  (local-unset-key (kbd "<return>"))
-  (let ((buffer (substring (buffer-name) 0 -10)) project)
-    (message "substring: %s" buffer)
-    (setq project (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+  (defun taskpaper-projectwindow-esc()
+    "Exit projectwindow, not selecting a project"
+    (interactive)
+    (local-unset-key (kbd "<return"))
+    (local-unset-key (kbd "<ESC> <ESC>"))
     (kill-buffer)
     (delete-window)
     (other-window (- 1))
-    (switch-to-buffer buffer)
-
-    (goto-char 0)
-    (re-search-forward project)
-    (taskpaper-unfocus-project)
-    (taskpaper-focus-selected-project)
-  
     )
-  )
 
-(defun taskpaper-focus-selected-project()
-  "Hide everything not related to project under cursor."
-  (interactive)
-  (let ((startpoint (point)) start end)
-    (end-of-line)
-    (re-search-backward ":$")
-    (beginning-of-line)
-    (setq start (point))
-    (re-search-forward ":$")
-    (re-search-forward ":$")
-    (beginning-of-line)
-    (setq end (point))
-    (add-text-properties 1 start '(invisible t))
-    (add-text-properties end (point-max) '(invisible t))
-    (goto-char startpoint)
-    )
-  )
+  (defun taskpaper-projectwindow-select()
+    "Action to perform when project is selected in project window."
+    (interactive)
+    (local-unset-key (kbd "<return>"))
+    (let ((buffer (substring (buffer-name) 0 -10)) project)
+      (message "substring: %s" buffer)
+      (setq project (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+      (kill-buffer)
+      (delete-window)
+      (other-window (- 1))
+      (switch-to-buffer buffer)
 
-(defun taskpaper-unfocus-project()
-  "Show all projects if focused on one."
-  (interactive)
-  (add-text-properties 1 (point-max) '(invisible nil))
-  )
-
-(defun taskpaper-toggle-done()
-  "Toggle done status on task, this sets @done-tag with date."
-  (interactive)
-  (let ((startpoint (point)) (line (line-number-at-pos)))
-    (re-search-forward "@done")
-    (if (= line (line-number-at-pos))
-        (progn
-          (end-of-line)
-          (re-search-backward "@")
-          (backward-char)
-          (kill-line)
-          )
-      (progn
-        (goto-char startpoint)
-        (end-of-line)
-        (insert " @done")
-        )
+      (goto-char 0)
+      (re-search-forward project)
+      (taskpaper-unfocus-project)
+      (taskpaper-focus-selected-project)
+      
       )
-    (goto-char startpoint)
+    )
+
+  (defun taskpaper-focus-selected-project()
+    "Hide everything not related to project under cursor."
+    (interactive)
+    (let ((startpoint (point)) start end)
+      (end-of-line)
+      (re-search-backward ":$")
+      (beginning-of-line)
+      (setq start (point))
+      (re-search-forward ":$")
+      (re-search-forward ":$")
+      (beginning-of-line)
+      (setq end (point))
+      (add-text-properties 1 start '(invisible t))
+      (add-text-properties end (point-max) '(invisible t))
+      (goto-char startpoint)
+      )
+    )
+
+  (defun taskpaper-unfocus-project()
+    "Show all projects if focused on one."
+    (interactive)
+    (add-text-properties 1 (point-max) '(invisible nil))
+    )
+
+  (defun taskpaper-toggle-done()
+    "Toggle done status on task, this sets @done-tag with date."
+    (interactive)
+    (let ((startpoint (point)) (line (line-number-at-pos)))
+      (back-to-indentation)
+      (re-search-forward "@done" nil 2)
+      (if (= line (line-number-at-pos))
+          (progn
+            (end-of-line)
+            (re-search-backward "@")
+            (backward-char)
+            (kill-line)
+            )
+        (progn
+          (goto-char startpoint)
+          (end-of-line)
+          (insert " @done")
+          )
+        )
+      (when (not (equal (point) (line-end-position)))
+        (goto-char startpoint))
+      )
     )
   )
-
 
 (defun taskpaper-mode ()
   "Major mode for editing taskpaper styled files."
@@ -136,9 +139,7 @@
   (setq mode-name "Taskpaper") ; for display purposes in mode line
   (use-local-map taskpaper-mode-map)
   
-  ;(copy-face 'font-lock-comment-face 'font-lock-taskpaper-done-face)
-  ;(set-face-attribute 'font-lock-taskpaper-done-face nil :strike-through t)
-
+  (taskpaper-functions)
   (setq font-lock-defaults '(tpKeywords))
   
   ;; Dont wrap lines
